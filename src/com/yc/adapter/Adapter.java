bd5c46186f8dec163a99326901987bf1e254b3aa
qqhbc <qqhbc1995@163.com>
2022-01-18 01:30:02 +0800

    feat: 适配器模式init

---
+++ src/com/yc/adapter/Adapter.java
@@ -0,0 +1,37 @@
+package com.yc.adapter;
+
+interface Target {
+    void request();
+}
+
+class Adaptee {
+    public void specificRequest() {
+        System.out.println("specificRequest");
+    }
+}
+
+/**
+ * 一种补偿模式</br>
+ * 对已运行项目的对象进行适配
+ */
+public class Adapter extends Adaptee implements Target {
+    @Override
+    public void request() {
+        super.specificRequest();
+    }
+}
+
+class Client {
+    public static void main(String[] args) {
+        Target target = new Target() {
+            @Override
+            public void request() {
+                System.out.println("Target request");
+            }
+        };
+        target.request();
+        System.out.println("===============================");
+        Target target1 = new Adapter();
+        target1.request();
+    }
+}
---
+++ src/com/yc/adapter/AdapterExtend.java
@@ -0,0 +1,49 @@
+package com.yc.adapter;
+
+class AdapteeExtend {
+    public void specificRequest() {
+        System.out.println("AdapteeExtend specificRequest");
+    }
+}
+
+class AdapteeExtend1 {
+    public void specificRequest1() {
+        System.out.println("AdapteeExtend1 specificRequest");
+    }
+}
+
+/**
+ * 通过组合解决不能多继承问题
+ */
+public class AdapterExtend implements Target {
+    private final AdapteeExtend adapteeExtend;
+    private final AdapteeExtend1 adapteeExtend1;
+
+    public AdapterExtend(AdapteeExtend adapteeExtend, AdapteeExtend1 adapteeExtend1) {
+        this.adapteeExtend = adapteeExtend;
+        this.adapteeExtend1 = adapteeExtend1;
+    }
+
+    @Override
+    public void request() {
+        adapteeExtend.specificRequest();
+        adapteeExtend1.specificRequest1();
+    }
+}
+
+class ClientExtend {
+    public static void main(String[] args) {
+        Target target = new Target() {
+            @Override
+            public void request() {
+                System.out.println("extend request");
+            }
+        };
+        target.request();
+        System.out.println("===========================");
+        AdapteeExtend adapteeExtend = new AdapteeExtend();
+        AdapteeExtend1 adapteeExtend1 = new AdapteeExtend1();
+        Target target1 = new AdapterExtend(adapteeExtend, adapteeExtend1);
+        target1.request();
+    }
+}
